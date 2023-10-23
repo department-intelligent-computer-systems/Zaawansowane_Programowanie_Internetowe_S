2b20922ea082b70b6ac313ed7a739fbf38f487f0
Jakub Sliwka <jakisemail@email.com>
2023-10-23 14:29:51 +0200

    lab3

---
+++ lab3/Ang1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  template: `
+   <app-first></app-first>,
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Ang1';
+}
---
+++ lab3/Ang1/src/app/first/first.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: 'first.component.html',
+  styleUrls: ['first.component.css'
+  ]
+})
+export class FirstComponent implements OnInit {
+  title: string = "Pierwsza wartość przekazana z komponentu";
+
+  constructor() { }
+  ngOnInit(): void { }
+
+  method() {
+    return "Wartość zwrócona z metody";
+    }
+
+  onClick() {
+    this.title = "Po kliknieciu";
+    }
+}
