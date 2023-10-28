bb3161268165600a807a3897e6fedf15b1068833
AwesomeSuperEpicMagicNinjaFreddy <dominiknowakfr@gmail.com>
2023-10-28 15:45:57 +0200

    Angular initial

--- src/Lab2/Zad3.ts
+++
@@ -1,4 +0,0 @@
-var array3: number[] = [1, 2, 3, 4, 5, 6];
-var threshold = 3;
-var result3 = array3.reduce((acc, curr) => curr > threshold ? acc + curr : acc, 0);
-export { result3 }
--- src/Lab2/zad1.ts
+++
@@ -1,3 +0,0 @@
-const array1: number[] = [1, 2, 3, 4, 5, 6];
-const result1 = array1.reduce((a, x) => a + x, 0)
-export { result1 };
