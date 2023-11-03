35db39915aad79cd7e6f362b08f8824eeb3f613f
Marek Sigmund <tmp@tmp.pl>
2023-11-03 12:53:32 +0100

    03.11

--- lab3/Ang1/src/app/galery/galery.component.ts
+++ lab3/Ang1/src/app/galery/galery.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class GaleryComponent {
 
   photos = [
-    'photo/1.png',
-    'photo/2.jpg',
-    'photo/3.webp',
+    './photo/1.png',
+    './photo/2.jpg',
+    './photo/3.webp',
    
   ];
   currentIndex: number = 0;
