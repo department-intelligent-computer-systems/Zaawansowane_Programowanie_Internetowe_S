56d6241777e21fbc9b9e7e43edb01c9ff45bfeac
Katarzyna <kasia@abc.pl>
2023-11-16 15:14:18 +0100

    Lab4-1

---
+++ react-app/src/Zad8.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import './App.css';
+
+const Gallery = () => {
+    const images = [
+        'https://img.wprost.pl/img/tatry-widoczne-z-sokolicy-w-pieninach/b1/a6/e58a6a258ba7f0a0bd2a46f63c87.jpeg',
+        'https://ocdn.eu/pulscms-transforms/1/C5Qk9kpTURBXy9kNTJiNDBmOWNjZjg4NTliZDAwNDAwMTFkYTMzNTlmMy5qcGeTlQPNBFkAzRaOzQywkwXNAxTNAbyTCaYxYTQzMmIG3gABoTAG/jesien-na-podhalu.avif',
+        'https://i.content4travel.com/cms/img/u/kraj/1/gory_0.jpg?version=1',
+        'https://1.bp.blogspot.com/-O5dxPSw-z5I/T56EjjoMYnI/AAAAAAAAHr0/XNInjmUb1L4/s1600/100_1187.JPG'
+    ];
+
+    const [currentImageIndex, setCurrentImageIndex]=useState(0);
+
+    const handleNextClick = () =>{
+        if (currentImageIndex <images.length -1){
+        setCurrentImageIndex(currentImageIndex+1);
+    } else {
+        alert('To jest ostatnie zdjęcie');
+    }
+};
+const handlePrevClick = () => {
+    if (currentImageIndex>0){
+        setCurrentImageIndex(currentImageIndex-1);
+    }else {
+        alert('To jest pierwsze zdjęcie');
+    }
+};
+
+const isFirstImage = currentImageIndex ===0;
+const isLastImage = currentImageIndex === images.length -1;
+return (
+    <div>
+        <img src={images[currentImageIndex]} alt={`Zdjecie ${currentImageIndex + 1}`} />
+    <div>
+        {!isFirstImage&&<button onClick={handlePrevClick}>Lewo</button>}
+        {!isLastImage&&<button onClick={handleNextClick}> Prawo</button>}
+    </div>
+    </div>
+);
+};
+function GalleryShow2(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Galleria Zdjęć</h1>
+                <Gallery />
+            </header>
+        </div>
+    );
+}
+export default GalleryShow2
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -7,13 +7,19 @@ import Zad2 from './Zad2';
 import Zad3 from './Zad3';
 import Zad4 from './Zad4';
 import Zad5 from './Zad5';
+import GalleryShow from './Zad6';
+import GalleryShow1 from './Zad7';
+import GalleryShow2 from './Zad8';
+
 
 ReactDOM.render(<App/>,document.getElementById('root'));
 ReactDOM.render(<Zad2/>,document.getElementById('root'));
 ReactDOM.render(<Zad3/>,document.getElementById('root'));
 ReactDOM.render(<Zad4/>,document.getElementById('root'));
 ReactDOM.render(<Zad5/>,document.getElementById('root'));
-
+ReactDOM.render(<GalleryShow/>,document.getElementById('root'));
+ReactDOM.render(<GalleryShow1/>,document.getElementById('root'));
+ReactDOM.render(<GalleryShow2/>,document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
