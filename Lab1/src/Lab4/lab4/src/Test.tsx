ac4ea01f6a79ce3ed0afab1a08b55c0b49438a2c
Jacek Pelikan <asd@asd.com>
2023-11-17 13:22:29 +0100

    17.11.2023

---
+++ Lab1/src/Lab4/lab4/src/Test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>asdasd</div>
+        <p>
+          Działa
+        </p>
+       
+      
+    </div>
+  );
+}
+
+export default App;
